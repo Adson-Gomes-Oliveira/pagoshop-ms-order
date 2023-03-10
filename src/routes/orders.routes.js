3407b49fc9ad3476b802660f175494385315343b
Adson Gomes Olivera <adsonprofessionalemail@gmail.com>
2023-03-10 09:08:19 -0300

    <feat> Created authentication for finance, order and product

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const passport = require('passport');
 const ordersController = require('../controllers/orders.controller');
 
 const router = Router();
 
+router.use('/', passport.authenticate('bearer', { session: false }));
 router.post('/', ordersController.create);
 router.post('/confirm/:id', ordersController.confirmOrder);
 
