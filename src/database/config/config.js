351f89165ee6dc2c77eaa890bbdf232079a38e8a
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-01 17:34:39 -0300

    <fix> Fixing several little problems

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,8 @@ module.exports = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'secret',
     database: process.env.DB_DATABASE || 'order',
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: process.env.DB_HOST || 'localhost',
+    port: '3306',
     dialect: process.env.DB_DIALECT || 'mysql',
   },
   test: {
